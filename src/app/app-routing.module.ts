0cf302e6e32db74ae6730590309bddeeca083bd3
juanjinario <juanjco2@gmail.com>
2023-03-04 13:23:09 +0100

    feat: add buttons & form pages structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'buttons',
+    loadChildren: () =>
+      import('./pages/buttons/buttons.module').then((m) => m.ButtonsModule),
+  },
+  {
+    path: 'form',
+    loadChildren: () =>
+      import('./pages/form/form.module').then((m) => m.FormModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
